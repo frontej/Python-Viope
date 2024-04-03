b1f1f46e049fd28291543196923569c8b693ac59
frontej <165875930+frontej@users.noreply.github.com>
2024-04-03 11:35:47 +0300

    7.2

--- Exercise_7.2.py
+++ Exercise_7.2.py
@@ -1,13 +1,12 @@
 #Toinen 4. luvun ohjelmointitehtävä liittyy avoimeen toistoon, jossa toistorakenne toteutetaan siten, että käyttäjän syötteet ratkaisevat sen milloin ohjelma lopetetaan.
-
 #Toteuta ohjelma, joka joka kierroksen alussa pyytää käyttäjältä merkkijonon ja tulostaa sen ruudulle. Ainoan poikkeuksen tähän tekee tilanne, jossa käyttäjä kirjoittaa tekstin "lopeta", jolloin ohjelma tulostaa tekstin "Lopetit ohjelman" ja sammuu. Toimiessaan oikein ohjelma tulostaa seuraavaa:
 
 #Kirjoita jotain: testi
-testi
-Kirjoita jotain: Urjala
-Urjala
-Kirjoita jotain: lopeta
-Lopetit ohjelman.
+#testi
+#Kirjoita jotain: Urjala
+#Urjala
+#Kirjoita jotain: lopeta
+#Lopetit ohjelman.
 
 #Ohjelma kannattaa sijoittaa yhden "while True:"-rakenteen sisään, ja katkaisuehto sitoa if-valintarakenteeseen ja break-käskyyn.
 #Example output:
