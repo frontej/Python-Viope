716eddb9254ec122959ad290aaba780f03935a9d
frontej <165875930+frontej@users.noreply.github.com>
2024-04-03 10:52:34 +0300

    6.1

---
+++ Exercise_6.1.py
@@ -0,0 +1,17 @@
+#Kolmannen luvun tehtävät aloitetaan yksinkertaisen if-rakenteen luomisella. Ohjelma pyytää käyttäjältä luvun ja tallentaa sen muuttujaan. Mikäli luku on parillinen, ohjelma tulostaa vastauksen "Antamasi luku oli parillinen". If-rakenteen ehtolauseeseen tarvittavat parametrit löydät luvun taulukosta, ja helpoin tapa toteuttaa ohjelma onkin käyttää jakojäännös-operaattoria, testaten onko jakojäännös 0. Ohjelman ei tarvitse myöskään reagoida virheellisiin syötteisiin eikä desimaalilukuihin. Ohjelma tulostaa seuraavan vastauksen:
+#Anna luku: 24
+#Antamasi luku oli parillinen.
+
+#tai vaihtoehtoisesti
+#Anna luku: 11
+
+#Eli ei mitään, mikäli luku ei ole parillinen.
+#Example output:
+#Anna luku: 11
+
+luku = input("Anna luku:")
+
+luku = int(luku)
+if luku%2 == 0:
+	print("Antamasi luku oli parillinen.")
+	
