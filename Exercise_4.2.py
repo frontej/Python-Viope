e97d2458157d51d766632c6513cb85bc626831ac
frontej <165875930+frontej@users.noreply.github.com>
2024-04-03 10:48:08 +0300

    4.2

---
+++ Exercise_4.2.py
@@ -0,0 +1,18 @@
+#Tässä tehtävässä harjoitellaan esimuotoillun merkkijonon (kolmen sitaatin merkkijonon) käyttämistä sekä tulosteen muotoilumerkkejä \n ja \t. Tee ohjelma, joka tulostaa seuraavan lauseen:
+#Tämä on usean rivin tulostus:
+#Teksti on jaettu usealle riville.
+#Nimi:	 Petteri
+#Ammatti: Kartturi
+
+
+#Ensimmäiset kaksi riviä voit muodostaa esimuotoiltua merkkijonoa käyttäen. Alempaa tulostetta varten joudut työskentelemään muotoilumerkkien kanssa.
+#Example output:
+#Tämä on usean rivin tulostus:
+#Teksti on jaettu usealle riville.
+#Nimi:	Petteri
+#Ammatti:	Kartturi
+
+print("""Tämä on usean rivin tulostus:
+Teksti on jaettu usealle riville.""")
+print("Nimi:\tPetteri")
+print("Ammatti:\tKartturi")
