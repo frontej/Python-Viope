527784aa6828cf336f01e4e80d9ba6449bccc4b9
frontej <165875930+frontej@users.noreply.github.com>
2024-04-03 11:43:53 +0300

    9.3

--- Exercise_9.3.py
+++ Exercise_9.3.py
@@ -1,6 +1,6 @@
 #Luvun kolmas tehtävä muokkaa eteenpäin luvun ensimmäistä tehtävää. Tällä kertaa tulostaja-funktion toimintaa muutetaan siten, että se vastaanottaa yhden parametrin, jonka oletusarvona on "Oletustulostus" ja tulostaa saamansa arvon.
 
-Pääfunktio taas pyytää käyttäjältä syötettä "Anna syöte (Lopeta lopettaa): ", ja mikäli käyttäjä antaa syötteen, joka on 5 tai useampi merkkiä, lähetetään se tulostaja-alifunktiolle. Muussa tapauksessa alifunktiota kutsutaan ilman parametriä. Jos käyttäjä antaa syötteen "Lopeta", ohjelma sammuu. Ohjelma toimii seuraavalla tavalla:
+#Pääfunktio taas pyytää käyttäjältä syötettä "Anna syöte (Lopeta lopettaa): ", ja mikäli käyttäjä antaa syötteen, joka on 5 tai useampi merkkiä, lähetetään se tulostaja-alifunktiolle. Muussa tapauksessa alifunktiota kutsutaan ilman parametriä. Jos käyttäjä antaa syötteen "Lopeta", ohjelma sammuu. Ohjelma toimii seuraavalla tavalla:
 
 #Anna syöte (Lopeta lopettaa): Pitkäsana
 #Pitkäsana
